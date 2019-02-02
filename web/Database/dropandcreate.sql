13066c986d8fd1a8dbdd121df27e959a350d2a76
Kieran Molloy <molloy.kieran@gmail.com>
2019-02-02 15:04:26 +1300

    DB recreation scripts

---
+++ web/Database/dropandcreate.sql
@@ -0,0 +1,14 @@
+﻿USE master
+IF EXISTS (SELECT * FROM sys.databases where name = N'DeliberatelyVulnerable')
+	EXEC msdb.dbo.sp_delete_database_backuphistory @database_name = N'DeliberatelyVulnerable'
+	GO
+
+	/* Query to Get Exclusive Access of SQL Server Database before Dropping the Database  */
+	ALTER DATABASE DeliberatelyVulnerable SET SINGLE_USER WITH ROLLBACK IMMEDIATE
+	GO
+
+	DROP DATABASE DeliberatelyVulnerable
+
+GO
+
+CREATE DATABASE DeliberatelyVulnerable
