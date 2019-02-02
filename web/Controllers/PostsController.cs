0e86ec027ee53bde6c06d706257ec76c997e549a
Kieran Molloy <molloy.kieran@gmail.com>
2019-02-02 15:04:05 +1300

    Adding entity framework support for DB access

---
+++ web/Controllers/PostsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using web.Entities;
+
+namespace web.Controllers
+{
+    public class PostsController : Controller
+    {
+        readonly MyDbContext _dbContext;
+
+        public PostsController(MyDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            var posts = _dbContext.Posts.Take(25).ToList();
+
+            return View(posts);
+        }
+    }
+}
