e97c0629d8049d0506aecdff84ebc9f32af7f285
Kieran Molloy <molloy.kieran@gmail.com>
2019-02-10 20:01:41 +1300

    Removed sql setup files from c# project

--- web/Database/data.sql
+++
@@ -1,5 +0,0 @@
-﻿INSERT INTO Posts (Html, Created)
-VALUES
-	('This is a test', '2019-01-05 15:32:45+13:00'),
-	('Yes, something is happening', '2019-01-05 16:21:12+13:00'),
-	('Bob was here', '2019-01-06 11:02:20+13:00')
--- web/Database/setup.ps1
+++
@@ -1,8 +0,0 @@
-﻿Import-Module sqlps
-
-$ConnectionString = "Server=localhost;Initial Catalog=DeliberatelyVulnerable;Trusted_Connection=true"
-$ConnectionStringMasterDb = "Server=localhost;Initial Catalog=master;Trusted_Connection=true"
-
-Invoke-SqlCmd -ConnectionString $ConnectionStringMasterDb -InputFile dropandcreate.sql
-Invoke-SqlCmd -ConnectionString $ConnectionString -InputFile tables.sql
-Invoke-SqlCmd -ConnectionString $ConnectionString -InputFile data.sql
