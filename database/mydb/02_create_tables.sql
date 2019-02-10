49f7ebf9d192ba6b27a4fd7b21bcddb3cab9de66
Kieran Molloy <molloy.kieran@gmail.com>
2019-02-10 20:22:35 +1300

    Added more dummy data

--- database/mydb/02_create_tables.sql
+++ database/mydb/02_create_tables.sql
@@ -4,3 +4,10 @@ CREATE TABLE CreditCard (
     expiry_month INT NOT NULL,
     expiry_year INT NOT NULL
 );
+
+CREATE TABLE History (
+    id SERIAL PRIMARY KEY,
+    location VARCHAR(256) NOT NULL,
+    date timestamp NOT NULL,
+    amount NUMERIC(14,2) NOT NULL
+);
